3deca89baebe88ceb7e5ee7d39ff1db84c1aaff1
john <john@example.com>
2012-12-10 21:49:25 -0800

    keys.java redo

--- src/com/android/locproof/stamp/Keys.java
+++ src/com/android/locproof/stamp/Keys.java
@@ -1,8 +1,9 @@
 package com.android.locproof.stamp;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import android.content.Context;
+import android.util.Log;
 
 
 public class Keys {
@@ -14,29 +15,36 @@ public class Keys {
 	public static byte[] MY_DSA_PRI_KEY;
 	public static byte[] CA_PUP_KEY;
 	
-	public static void initKeys(){
-		FileInputStream keyfis;
-		try {
-			keyfis = new FileInputStream("keys\\DSAPubKey"+DSA_SIZE);
-			MY_DSA_PUP_KEY = new byte[keyfis.available()];
-			keyfis.close();
-			
-			keyfis = new FileInputStream("keys\\DSAPriKey"+DSA_SIZE);
-			MY_DSA_PRI_KEY = new byte[keyfis.available()];
-			keyfis.close();
-			
-			keyfis = new FileInputStream("keys\\RSAPubKey"+RSA_SIZE);
-			CA_PUP_KEY = new byte[keyfis.available()];
-			keyfis.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+	/**
+	 * Read key content into an byte array from key file
+	 * @param aKeyFile key file
+	 * @return byte array storing key content
+	 */
+	private static byte[] readKeyFile(String aKeyFile){
+		File keyFile = new File(aKeyFile);
+		byte[] data = new byte[(int) keyFile.length()];
+		try{
+			new FileInputStream(keyFile).read(data);
+		}catch (Exception e){
 			e.printStackTrace();
 		}
-		
+		return data;
 	}
 	
+	public static void initKeys(Context aContext){
+		String keyPath = aContext.getExternalFilesDir(null).getAbsolutePath();
+		String filePath;
+
+		filePath = keyPath+"/keys/DSAPubKey"+DSA_SIZE;
+		MY_DSA_PUP_KEY = readKeyFile(filePath);
+		Log.d("KEYS", "DSAPUB: "+new String(MY_DSA_PUP_KEY)+" bytes: "+MY_DSA_PUP_KEY.length);
+		
+		filePath = keyPath+"/keys/DSAPriKey"+DSA_SIZE;
+		MY_DSA_PRI_KEY = readKeyFile(filePath);
+		Log.d("KEYS", "DSAPRI: "+new String(MY_DSA_PRI_KEY)+" bytes: "+MY_DSA_PRI_KEY.length);
+		
+		filePath = keyPath+"/keys/RSAPubKey"+RSA_SIZE;
+		CA_PUP_KEY = readKeyFile(filePath);
+		Log.d("KEYS", "CAKEY: "+new String(CA_PUP_KEY)+" bytes: "+CA_PUP_KEY.length);
+	}
 }
